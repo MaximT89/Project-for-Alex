caa6ced24df1d7f7a7a33fd5757797a40cebe61c
BuenasDias <tmaksim1989@gmail.com>
2022-05-31 21:41:32 +0300

    Chapter 1 create

--- app/src/main/java/com/max/interview/testbdforalex/presentation/screens/listUsers/MainActivity.kt
+++ app/src/main/java/com/max/interview/testbdforalex/presentation/screens/listUsers/MainActivity.kt
@@ -1,11 +1,17 @@
-package com.max.interview.testbdforalex
+package com.max.interview.testbdforalex.presentation.screens.listUsers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.max.interview.testbdforalex.R
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
